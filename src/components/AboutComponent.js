a6a54ba330f7e882e866b893f100e604af53add8
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2021-07-22 10:41:58 -0400

    Router and content Added

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,7 +3,46 @@ import React from 'react';
 function About() {
 	return (
 		<div>
-			<h1>YOOOOOLLLLLOOOOOO</h1>
+			<h1>About Me</h1>
+			<p>
+				I believe in creating a lifestyle that works FOR YOU, not
+				forcing yourself into habits that you hate or won’t serve you. I
+				use science to create a customized, evidence based plan.
+				Everyone’s plan looks different because everyone’s lifestyle,
+				goals, genetics, limitations, hormones, fitness levels, etc are
+				different. This is typically done in a 3 step process of 1:
+				upregulating your metabolism and building a healthy base, 2:
+				cutting body fat at a sustainable rate, and then 3: creating
+				balance in the maintenance phase. I use a holistic approach to
+				help reach your goals being that health uses 3 pillars. We work
+				on your fitness, diet, AND mindset. You need all 3 to be
+				successful in reaching your goals.
+			</p>
+			<p>
+				My clients’ success is my success and I put my heart and soul
+				into all my clients. My passion is to help people beyond their
+				time with me and instill them with the habits to keep them
+				successful for the rest of their life. There is no point in
+				doing a pushy, quick fix program to lose some weight, only to
+				have no idea what to do when it’s over, and go back to where you
+				were before or rebound even worse. If the program is not
+				sustainable you will never ever have the body or life you
+				desire. I want to put an end to yo-yo dieting and all-or-nothing
+				mentalities. Your health and well being will come before
+				ANYTHING else and I do everything with YOUR best interest in
+				mind.
+			</p>
+			<p>
+				I’ve lost weight, I’ve gained weight, I’ve been unhealthy, I’ve
+				been where you are.. and if I haven’t personally experienced
+				what you’re going through one of the hundreds of amazing clients
+				I have worked with has. No matter where you are in life or how
+				far out of reach your goals may seem I guarantee I can get you
+				to where you want to be if you follow my plan. You need
+				accountability and direction to get there and that’s just a
+				piece of what I provide. Stop putting your life, happiness, and
+				health on hold and take charge of it today!
+			</p>
 		</div>
 	);
 }
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import '../styles/home.css';
 
 function Home() {
-	return <div>HOME</div>;
+	return (
+		<div>
+			<h1>EXPERT HEALTH & FITNESS COACH</h1>
+			<h2>Online Health Coach | Personal Trainer | NPC Posing Coach</h2>
+			<ul>
+				<li>
+					B.S. Michigan State University: Kinesiology, Specialization-
+					Exercise Science, Cognates- Fitness Leadership & Health
+					Promotion
+				</li>
+				<li>
+					ACSM CPT: American College Sports Medicine Certified
+					Personal Trainer
+				</li>
+				<li>
+					IFBBPro: International Federation of Bodybuilding
+					Professional Bikini Competitor
+				</li>
+				<li>Experience: 9+ yrs</li>
+			</ul>
+		</div>
+	);
 }
 
 export default Home;
