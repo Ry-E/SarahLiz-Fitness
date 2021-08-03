53a351380612498f2d5d6bc882f1f984d7a25861
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2021-08-03 18:55:27 -0400

    refactoring

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -7,21 +7,17 @@ function Home() {
 		<div className="home">
 			<div className="home_content">
 				<h1 className="headline">Expert Health & Fitness Coach</h1>
-				<div className="portrait_container">
-					<img
-						className="portrait"
-						src={portrait}
-						alt="A friendly looking trainer"
-					/>
-				</div>
+				<img
+					className="portrait"
+					src={portrait}
+					alt="A friendly looking trainer"
+				/>
 				<h2 className="services">
 					Online Health Coach &#9900; Personal Trainer &#9900; NPC
 					Posing Coach
 				</h2>
-				<div className="info_container">
-					<div className="info">
-						<p>"We work on your fitness, diet, AND mindset."</p>
-					</div>
+				<div className="info">
+					<p>"We work on your fitness, diet, AND mindset."</p>
 				</div>
 			</div>
 		</div>
