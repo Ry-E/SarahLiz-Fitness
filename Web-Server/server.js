5bab67a2e9ff9fa162a11511c97c82fa7a5ddeff
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2021-10-05 15:44:07 -0400

    Heroku test deploy

--- Web-Server/server.js
+++ Web-Server/server.js
@@ -26,7 +26,7 @@ let transporter = nodemailer.createTransport({
 
 let myPath = '/Users/Adob3/Desktop/NucampFolder/sarahliz-fitness/build';
 // const file = new statik.Server(myPath);
-const createFileLoc = () => {
+const createFileLoc = req => {
 	let staticBasePath = './build';
 	let resolvedBase = path.resolve(staticBasePath);
 	let safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '');
@@ -67,22 +67,25 @@ const staticServe = function (req, res) {
 			}
 		});
 	} else if (req.url.match('.css$')) {
-		createFileLoc();
+		createFileLoc(req);
 		let cssPath = path.join(fileLoc, req.url);
 		var fileStream = fs.createReadStream(cssPath, 'UTF-8');
 		res.writeHead(200, { 'Content-Type': 'text/css' });
 		fileStream.pipe(res);
 	} else if (req.url.match('.js$')) {
+		createFileLoc(req);
 		let jsPath = path.join(fileLoc, req.url);
 		var fileStream = fs.createReadStream(jsPath, 'UTF-8');
 		res.writeHead(200, { 'Content-Type': 'text/javascript' });
 		fileStream.pipe(res);
 	} else if (req.url.match('.png$')) {
+		createFileLoc(req);
 		let pngPath = path.join(fileLoc, req.url);
 		var fileStream = fs.createReadStream(pngPath);
 		res.writeHead(200, { 'Content-Type': 'image/png' });
 		fileStream.pipe(res);
 	} else if (req.url.match('.jpeg$')) {
+		createFileLoc(req);
 		let jpegPath = path.join(fileLoc, req.url);
 		var fileStream = fs.createReadStream(jpegPath);
 		res.writeHead(200, { 'Content-Type': 'image/jpeg' });
